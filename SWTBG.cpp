02e702bc9382d9f5345caee67cdf28c0482d4247
Alejandro Morel <amorel11@fordham.edu>
2024-10-03 22:14:08 -0400

    Update SWTBG.cpp
    updated 10/03/24
    
    added setters and getters 
    simplified some formulas and code

--- SWTBG.cpp
+++ SWTBG.cpp
@@ -12,7 +12,7 @@
 
 // function prototypes
 //function used to simulate a fight between 2 characters
-void duel (Character& user, Character& opponenet);
+void duel (Character& user, Character& opponenet); 
 
 
 
@@ -21,23 +21,23 @@ int main () {
     //allows random numbers to be generated
     //used to randomize opponent moves
     srand(static_cast <unsigned int> (time(0)));
-
+    
     std::string username;       //player can pick a username
-
+    
     std::cout<<"Enter your name..."<<std::endl;
-    std::cin>> username;
-
+    std::cin>> username; 
+    
     Character MyPlayer(username);           //creates user's character
     Character Vader("Darth Vader");         //creates Darth Vader
-
+    
     //sets pointers to dynamically allocated abilities
-    Ability* meditation = new ForceMeditation();
-    Ability* strike = new SaberStrike();
+    Ability* meditation = new ForceMeditation(); 
+    Ability* strike = new SaberStrike(); 
     Ability* lightning = new ForceLightning();
-    Ability* counter = new Counter();
+    Ability* counter = new Counter(); 
 
     //characters learn abilities
-    MyPlayer.learnAbility(strike);
+    MyPlayer.learnAbility(strike); 
     MyPlayer.learnAbility(lightning);
     MyPlayer.learnAbility(counter);
     MyPlayer.learnAbility(meditation);
@@ -45,9 +45,9 @@ int main () {
 
     //shows the user their stats
     MyPlayer.displayStats();
-
+    
     //text art of Darth vader
-    std::cout<<".                     .-.                   "<<std::endl;
+    std::cout<<".                     .-.                   "<<std::endl; 
     std::cout<<"                     |_:_|                  "<<std::endl;
     std::cout<<"                    /(_Y_)\\                "<<std::endl;
     std::cout<<"                   ( \\/M\\/ )              "<<std::endl;
@@ -72,20 +72,20 @@ int main () {
 
 
     //simulates a duel between the user and darth vader
-    duel (MyPlayer, Vader);
-
-
-
-
-
-
+    duel (MyPlayer, Vader); 
+    
+    
+    
+    
+    
+    
     //deallocates memory of abilities
-    delete meditation;
-    delete strike;
-    delete lightning;
+    delete meditation; 
+    delete strike; 
+    delete lightning; 
+    
 
-
-    return 0;
+    return 0; 
 }
 
 //function definitions
@@ -96,53 +96,53 @@ void duel (Character& user, Character& opponent) {
     bool validity = false;  //ensures user picks a valid ability
 
     //while loop ensures duel ends when user or opponent dies
-    while (user.health > 0 && opponent.health > 0) {
-
+    while (user.getHealth() > 0 && opponent.getHealth() > 0) { 
+    
         //user goes when turn counter is even
         //turn counter starts at 0 allowing user to go first
         if (turnCounter%2 == 0) {
-
+           
             while (!validity) {
                 user.displayAbilities();        //helps user choose ability
-                std::cout<< "Choose Ability: ";
+                std::cout<< "Choose Ability: "; 
                 std::cin>> index;               //user's chosen ability
-
+                
                 //verifies index is valid
-                if (index > 0 && index <= user.abilities.size()) {
+                if (index > 0 && index <= user.getNumOfAbilities()) {
                     validity = true;  //ends loop if valid ability is chosen
                 }
                 else {
                     std::cout<<"Invalid selection. Choose Ability"<<std::endl;
                 }
-
+                
+                std::cout<<std::endl; 
                 std::cout<<std::endl;
-                std::cout<<std::endl;
-
+                
             }
             validity = false;       //allows loop to restart next turn
             user.useAbility(index, opponent);   //uses chosen ability
-
+           
             turnCounter++;          //gives opponent the next turn
         }
         else {          //opponent moves
             index = 1 + rand() % 1;     //random number generate chooses move
-
+            
             opponent.useAbility(index, user);   //uses move
-
+            
             turnCounter++;              //next turn
         }
-
+        
     }
-
-    if (user.health <= 0) {                     //message when user is defeated
+    
+    if (user.getHealth() <= 0) {                     //message when user is defeated
         std::cout<<"You died!"<<std::endl;
-        std::cout<<std::endl;
+        std::cout<<std::endl; 
         std::cout<<std::endl;
     }
     else {              //message when opponent is defeated
-        std::cout<<opponent.characterName<< " died!"<<std::endl;
-        std::cout<<std::endl;
+        std::cout<<opponent.getName()<< " died!"<<std::endl;
+        std::cout<<std::endl; 
         std::cout<<std::endl;
     }
 };
-                                         
+
